6f6f0bc5293a5383bcf5c969ac0989bfaa612c1e
Simone Pucci <spucci@byte-code.com>
2015-09-07 15:20:33 +0200

    Added consistency checks

--- dockerClean.sh
+++ dockerClean.sh
@@ -1,16 +1,46 @@
 #!/bin/bash
 
-#set -o errexit
+set -eou pipefail
+
+#usage: sudo ./dockerClean.sh [--dry-run]
+dryrun=false
+verbose=false
+docker_bin=$(which docker.io 2> /dev/null || which docker 2> /dev/null)
+
+if [ -z "$docker_bin" ] ; then
+    echo "Please install docker. You can install docker by running \"wget -qO- https://get.docker.io/ | sh\"."
+    exit 1
+fi
+
+while [[ $# > 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -n|--dry-run)
+            dryrun=true
+        ;;
+        *)
+            echo "Cleanup docker containers and images: remove unused containers and images."
+            echo "Usage: ${0##*/} [--dry-run]"
+            echo "   -n, --dry-run: dry run: display what would get removed."
+            exit 1
+        ;;
+    esac
+    shift
+done
+
+[ "${dryrun}" = true ] && docker_bin=echo
 
 echo "Removing exited docker containers..."
-docker ps -a -f status=exited -q | xargs -r docker rm -v
+${docker_bin} ps -a -f status=exited -q | xargs -r ${docker_bin} rm -v
 
 echo "Removing dangling images..."
-docker images --no-trunc -q -f dangling=true | xargs -r docker rmi
+${docker_bin} images --no-trunc -q -f dangling=true | xargs -r ${docker_bin} rmi
 
 echo "Removing unused docker images"
-images=($(docker images | tail -n +2 | awk '{print $1":"$2}'))
-containers=($(docker ps -a | tail -n +2 | awk '{print $2}'))
+images=($(${docker_bin} images | tail -n +2 | awk '{print $1":"$2}'))
+containers=($(${docker_bin} ps -a | tail -n +2 | awk '{print $2}'))
 
 containers_reg=" ${containers[*]} "
 remove=()
@@ -23,5 +53,6 @@ done
 
 remove_images=" ${remove[*]} "
 
-echo ${remove_images} | xargs -r docker rmi 
+echo ${remove_images} | xargs -r ${docker_bin} rmi || echo "Some errors happened while deleting unused images, check the logs for details."
+
 echo "Done"
