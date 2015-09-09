4c69d315a9cdd8f18c3f497e91713040c9c7df90
Simone Pucci <spucci@byte-code.com>
2015-09-09 11:58:35 +0200

    Modified Image cancellation section, in order to exlude images actually used by some container

--- dockerClean.sh
+++ dockerClean.sh
@@ -60,30 +60,67 @@ else
 fi
 
 echo "Removing unused docker images..."
-images=($(${docker_bin} images | tail -n +2 | awk '{print $1":"$2}'))
-containers=($(${docker_bin} ps -a | tail -n +2 | awk '{print $2}'))
+ToBeCleanedImageIdList="/dev/shm/ToBeCleanedImageIdList"
+ContainerImageIdList="/dev/shm/ContainerImageIdList"
+ImageIdList="/dev/shm/ImageIdList"
+ImageFullList="/dev/shm/ImageFullList"
+rm -f ${ToBeCleanedImageIdList} ${ContainerImageIdList} ${ImageIdList} ${ImageFullList}
 
-containers_reg=" ${containers[*]} "
-remove=()
+# Get all image ID
+${docker_bin} images -q --no-trunc | sort -o ${ImageIdList}
+CONTAINER_ID_LIST=$(${docker_bin} ps -aq --no-trunc)
+${docker_bin} images --no-trunc | tail -n +2 | awk '{print $3" "$1":"$2}' > ${ImageFullList}
 
-for item in ${images[@]}; do
-  if [[ ! $containers_reg =~ " $item " ]]; then
-    remove+=($item)
-    if [ "${dryrun}" == false -a ! -z "${item}" ];
-    then
-        echo "Deleting docker unused image: ${line}";
-        ${docker_bin} rmi ${item} || echo "Some errors happened while deleting unused image: ${item}, check the logs for details.";
-    fi
-  fi
+# Get Image ID that is used by a containter
+rm -f ${ContainerImageIdList}
+touch ${ContainerImageIdList}
+for CONTAINER_ID in ${CONTAINER_ID_LIST}; do
+    LINE=$(${docker_bin} inspect ${CONTAINER_ID} | grep "\"Image\": \"[0-9a-fA-F]\{64\}\"")
+    IMAGE_ID=$(echo ${LINE} | awk -F '"' '{print $4}')
+    echo "${IMAGE_ID}" >> ${ContainerImageIdList}
 done
+sort ${ContainerImageIdList} -o ${ContainerImageIdList}
 
-if [ ${#remove[@]} -gt 0 ];
-then
-    remove_images=" ${remove[*]} "
+# Remove the images being used by cotnainers from the delete list
+comm -23 ${ImageIdList} ${ContainerImageIdList} > ${ToBeCleanedImageIdList}
+
+cat ${ToBeCleanedImageIdList} | while read line;
+do
     if [ "${dryrun}" == true ];
     then
-        echo ${remove_images} | xargs -r echo "The following unused images would be deleted: "
+        echo "The following unused images would be deleted: ";
+        grep ${line} ${ImageFullList} | awk '{print $2}';
+    else
+        echo "Deleting docker unused image: ${line}";
+        ${docker_bin} rmi ${line} || echo "Some errors happened while deleting dangling image: ${line}.";
     fi
-fi
+done
+
+
+#images=($(${docker_bin} images | tail -n +2 | awk '{print $1":"$2}'))
+#containers=($(${docker_bin} ps -a | tail -n +2 | awk '{print $2}'))
+#
+#containers_reg=" ${containers[*]} "
+#remove=()
+#
+#for item in ${images[@]}; do
+#  if [[ ! $containers_reg =~ " $item " ]]; then
+#    remove+=($item)
+#    if [ "${dryrun}" == false -a ! -z "${item}" ];
+#    then
+#        echo "Deleting docker unused image: ${line}";
+#        ${docker_bin} rmi ${item} || echo "Some errors happened while deleting unused image: ${item}, check the logs for details.";
+#    fi
+#  fi
+#done
+#
+#if [ ${#remove[@]} -gt 0 ];
+#then
+#    remove_images=" ${remove[*]} "
+#    if [ "${dryrun}" == true ];
+#    then
+#        echo ${remove_images} | xargs -r echo "The following unused images would be deleted: "
+#    fi
+#fi
 
 echo "Done"
