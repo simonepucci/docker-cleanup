755d30783e861c0e000be4e566e72f13ddc65d52
Simone Pucci <spucci@byte-code.com>
2016-02-29 10:20:01 +0100

    Fixed path

--- docker-bug-finder.sh
+++ docker-bug-finder.sh
@@ -33,7 +33,7 @@ ssh -i ~/.ssh/deis core@${TUNHOST} "fleetctl list-units|grep -Eo '([0-9]{1,3}\.)
 for IPADDRESS in $(cat /dev/shm/ip);
 do
     BUG="";
-    BUG=$( ssh -i .ssh/deis core@$IPADDRESS 'journalctl -r | grep -m 1 -Ei "Failed to allocate and map port: iptables failed|Could not generate persistent MAC address for .*: No such file or directory|Daemon has completed initialization"
+    BUG=$( ssh -i ~/.ssh/deis core@$IPADDRESS 'journalctl -r | grep -m 1 -Ei "Failed to allocate and map port: iptables failed|Could not generate persistent MAC address for .*: No such file or directory|Daemon has completed initialization"
 ' )
     [ -z "${BUG}" ] && continue;
     echo "${BUG}" | grep -q "Daemon has completed initialization" && msg "Docker Was restarted on Host: ${IPADDRESS}" || msg "Docker Bug is Affecting Host: ${IPADDRESS} - ${BUG}";
