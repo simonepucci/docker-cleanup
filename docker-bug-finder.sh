115782a1ee6d3554d3c3684c1a6b65d8f7b028f5
Simone Pucci <spucci@byte-code.com>
2016-02-29 11:26:46 +0100

    docker-bug-finder.sh was changed into a local machine script

--- docker-bug-finder.sh
+++ docker-bug-finder.sh
@@ -2,7 +2,6 @@
 #
 #
 #
-#DISCLAIMER    This script is intended to be run from ctl-machine.
 #DISCLAIMER    Run docker check parsing system logs
 #DISCLAIMER    Usage Options: [-nh ]
 #DISCLAIMER        -n: dry run: display only what would get removed.
@@ -25,19 +24,11 @@ do
                 ;;
         esac
 done
-[ $# -gt 2 ] && usage || IPADDRESS="$(eval "echo -n \$$#")";
-TUNHOST=$(echo ${IPADDRESS} | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}');
-[ -z "${TUNHOST}" ] && { echo -e "Missing required argument.\nUsage: $0 IPADDRESS"; exit 1; }
 
-ssh -i ~/.ssh/deis core@${TUNHOST} "fleetctl list-units|grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}'" | sort | uniq | xargs > /dev/shm/ip
-for IPADDRESS in $(cat /dev/shm/ip);
-do
-    BUG="";
-    BUG=$( ssh -i ~/.ssh/deis core@$IPADDRESS 'journalctl -r | grep -m 1 -Ei "Failed to allocate and map port: iptables failed|Could not generate persistent MAC address for .*: No such file or directory|Daemon has completed initialization"
-' )
-    [ -z "${BUG}" ] && continue;
-    echo "${BUG}" | grep -q "Daemon has completed initialization" && msg "Docker Was restarted on Host: ${IPADDRESS}" || msg "Docker Bug is Affecting Host: ${IPADDRESS} - ${BUG}";
-done
+BUG="";
+BUG=$( journalctl -r | grep -m 1 -Ei "Failed to allocate and map port: iptables failed|Could not generate persistent MAC address for .*: No such file or directory|Daemon has completed initialization" )
+[ -z "${BUG}" ] && exit 0;
+echo "${BUG}" | grep -q "Daemon has completed initialization" && msg "Docker Was restarted on Host: ${IPADDRESS}" || msg "Docker Bug is Affecting Host: ${IPADDRESS} - ${BUG}";
 
 #journalctl -r | grep -m 1 -Ei "Failed to allocate and map port: iptables failed|Could not generate persistent MAC address for .*: No such file or directory|Daemon has completed initialization"
 
