f8f71f6cc5c7e2ba1956c67190ff8fcebeb1a2f5
Simone Pucci <spucci@byte-code.com>
2016-07-01 17:49:40 +0200

    Fixed docker-fill-tmprun-files.sh

--- docker-fill-tmprun-files.sh
+++ docker-fill-tmprun-files.sh
@@ -4,11 +4,9 @@
 #	Fill /tmp/RUN and /tmp/RUNPRE files with stop and start deis-builder.service
 cat > /tmp/RUNPRE <<EOF
 stop deis-builder.service
-
 EOF
 
 cat > /tmp/RUN <<EOF
 start deis-builder.service
-
 EOF
 
